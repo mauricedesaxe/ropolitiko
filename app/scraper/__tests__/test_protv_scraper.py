65afb856624f2a22ca3948fcee1c625c6999491e
lazaralex98 <alex2012ro@gmail.com>
2025-05-12 20:44:09 +0300

    test download with more articles

--- app/scraper/__tests__/test_protv_scraper.py
+++ app/scraper/__tests__/test_protv_scraper.py
@@ -58,16 +58,27 @@ def test_scrape_article_content():
     
     # If API is working, test the content scraper
     scraper = ProTVArticleScraper()
-    test_url = "https://stirileprotv.ro/stiri/actualitate/ce-spun-ungurii-despre-rezultatul-alegerilor-prezidentiale-din-romania-l-au-descris-pe-george-simion-intr-un-singur-cuvant.html"
-    content = scraper.scrape_article_content(test_url)
-    
-    # Verify we got content
-    assert isinstance(content, str)
-    assert len(content) > 0, "No article content found"
-    
-    # Print sample of content for debugging
-    print(f"Content sample: \n\n{content[:250]}...\n\n")
+    test_urls = [
+        "https://stirileprotv.ro/stiri/actualitate/ce-spun-ungurii-despre-rezultatul-alegerilor-prezidentiale-din-romania-l-au-descris-pe-george-simion-intr-un-singur-cuvant.html",
+        "https://stirileprotv.ro/stiri/actualitate/cine-este-george-simion-castigator-in-primul-tur-la-alegerile-prezidentiale-2025-de-la-activist-la-favorit-la-cotroceni.html",
+        "https://stirileprotv.ro/alegeri/prezidentiale/2024/basescu-critica-dur-diaspora-zei-traiesc-in-occident-si-spun-voi-care-traiti-acasa-ia-treceti-catre-tatucu-putin.html",
+        "https://stirileprotv.ro/alegeri/prezidentiale/2024/kremlinul-si-a-anuntat-oficial-sprijinul-pentru-george-simion-mesajul-fantasmagoric-transmis-de-ideologul-lui-putin.html",
+        "https://stirileprotv.ro/stiri/financiar/dezastru-pe-piata-valutara-cursul-oficial-anuntat-de-bnr.html"
+    ]
 
-    # write file to ./raw_content_samples/ce-spun-ungurii.txt
-    with open("app/scraper/__tests__/raw_content_samples/ce-spun-ungurii.txt", "w") as f:
-        f.write(content)
+    for test_url in test_urls:
+        content = scraper.scrape_article_content(test_url)
+        
+        # Verify we got content
+        assert isinstance(content, str)
+        assert len(content) > 0, "No article content found"
+        
+        # Print sample of content for debugging
+        print(f"Content sample: \n\n{content[:250]}...\n\n")
+
+        # extract title from url
+        title = test_url.split("/")[-1].replace(".html", "")
+
+        # write file to ./raw_content_samples/ce-spun-ungurii.txt
+        with open(f"app/scraper/__tests__/raw_content_samples/{title}.txt", "w") as f:
+            f.write(content)
